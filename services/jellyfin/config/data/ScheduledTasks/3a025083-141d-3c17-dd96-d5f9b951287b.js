2e080a8ea1ccdbc58ac6f889417921596c28b2ad
vladgheorghi <gheorghiuvlad21@gmail.com>
2025-07-25 17:38:35 +0300

    Removed version from compose as is deprecated

---
+++ services/jellyfin/config/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-07-25T14:36:16.3613249Z","EndTimeUtc":"2025-07-25T14:36:16.3657751Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
---
+++ services/jellyfin/config/data/ScheduledTasks/7d8088c1-0902-f1bf-4072-ded42437bcfb.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-07-25T14:36:16.3613751Z","EndTimeUtc":"2025-07-25T14:36:16.3638093Z","Status":"Completed","Name":"Clean Transcode Directory","Key":"DeleteTranscodeFiles","Id":"7d8088c10902f1bf4072ded42437bcfb"}
